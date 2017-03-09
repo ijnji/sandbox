525c8215db2cc607eebd7e995f2e5849b87ade26
ijnji <rnduan@gmail.com>
2017-03-09 04:40:08 -0500

    Update.

--- java/epi/ScoreCombination.java
+++ java/epi/ScoreCombination.java
@@ -17,18 +17,39 @@ import static rlib.Testing.expect;
 public class ScoreCombination {
 
   public static int combinations(int totalScore, List<Integer> playScores) {
-    return 0;
+    List<List<Integer>> memo = new ArrayList<>();
+    for (int i = 0; i < playScores.size(); i++) {
+      List<Integer> row = new ArrayList<Integer>();
+      memo.add(row);
+      for (int j = 0; j <= totalScore; j++) {
+        int c = 0;
+        if (i == 0) {
+          c = (j % playScores.get(0) == 0) ? 1 : 0;
+        } else {
+          List<Integer> prow = memo.get(i - 1);
+          for (int k = j; k >= 0; k -= playScores.get(i)) {
+            c += prow.get(k);
+          }
+        }
+        row.add(c);
+      }
+    }
+    return memo.get(playScores.size() - 1).get(totalScore);
   }
 
   public static void smallTest() {
     List<Integer> playScores;
     playScores = Arrays.asList(2);
-    expect(combinations(2, playScores)).toBe(1);
     expect(combinations(1, playScores)).toBe(0);
+    expect(combinations(2, playScores)).toBe(1);
     playScores = Arrays.asList(2, 3, 7);
-    expect(combinations(12, playScores)).toBe(4);
-    expect(combinations(9, playScores)).toBe(3);
     expect(combinations(5, playScores)).toBe(1);
+    expect(combinations(9, playScores)).toBe(3);
+    expect(combinations(12, playScores)).toBe(4);
+    playScores = Arrays.asList(1, 5, 10, 25);
+    expect(combinations(5, playScores)).toBe(2);
+    expect(combinations(10, playScores)).toBe(4);
+    expect(combinations(50, playScores)).toBe(49);
   }
 
   public static void main(String[] args) {
