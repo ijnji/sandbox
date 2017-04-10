d2197b58ac6bdd2457d4570c6c59c8a6c1edea5d
ijnji <rnduan@gmail.com>
2017-04-10 06:15:03 -0400

    Update.

--- java/TreeTraversals.java
+++ java/TreeTraversals.java
@@ -1,43 +1,56 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.LinkedList;
-import java.util.ArrayList;
 
 import rlib.BinaryTreeNode;
+import rlib.TreeUtils;
 import static rlib.TestingUtils.expect;
 
 public class TreeTraversals {
 
-  private static void inorderRecursive(BinaryTreeNode<Integer> root) {
+  private static void inorderRecursive(BinaryTreeNode<Integer> root, List<Integer> result) {
     if (root == null) return;
-    inorderRecursive(root.left);
-    if (root.data != null) System.out.println(root.data);
-    else System.out.println("null");
-    inorderRecursive(root.right);
+    inorderRecursive(root.left, result);
+    result.add(root.data);
+    inorderRecursive(root.right, result);
+  }
+
+  private static void preorderRecursive(BinaryTreeNode<Integer> root, List<Integer> result) {
+    if (root == null) return;
+    result.add(root.data);
+    preorderRecursive(root.left, result);
+    preorderRecursive(root.right, result);
+  }
+
+  private static void postorderRecursive(BinaryTreeNode<Integer> root, List<Integer> result) {
+    if (root == null) return;
+    postorderRecursive(root.left, result);
+    postorderRecursive(root.right, result);
+    result.add(root.data);
   }
 
   public static void main(String[] args) {
-    //       3
-    //     9   20
-    //    n n 15 7
-    // n, 9, n, 3, 15, 20, 7
-    List<Integer> dts = Arrays.asList(3, 9, 20, null, null, 15, 7);
-    BinaryTreeNode<Integer> root = new BinaryTreeNode<>(dts.get(0));
-    Queue<BinaryTreeNode<Integer>> que = new LinkedList<>();
-    BinaryTreeNode<Integer> n = root;
-    for (int i = 1; i < dts.size(); i++) {
-      if (n.left == null) {
-        n.left = new BinaryTreeNode<>(dts.get(i));
-        que.add(n.left);
-      } else {
-        n.right = new BinaryTreeNode<>(dts.get(i));
-        que.add(n.right);
-        n = que.remove();
-      }
-    }
+    // 3
+    //  |--20
+    //  |   |--7
+    //  |   |--15
+    //  |--9
+    //      |--n
+    //      |--n
 
-    inorderRecursive(root);
+    BinaryTreeNode<Integer> root = TreeUtils.buildTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
+    List<Integer> res;
+    res = new ArrayList<Integer>();
+    inorderRecursive(root, res);
+    expect(res).toBe(Arrays.asList(null, 9, null, 3, 15, 20, 7));
+    res = new ArrayList<Integer>();
+    preorderRecursive(root, res);
+    expect(res).toBe(Arrays.asList(3, 9, null, null, 20, 15, 7));
+    res = new ArrayList<Integer>();
+    postorderRecursive(root, res);
+    expect(res).toBe(Arrays.asList(null, null, 9, 15, 7, 20, 3));
   }
   
 }
--- java/rlib/TreeUtils.java
+++ java/rlib/TreeUtils.java
@@ -1,5 +1,28 @@
 package rlib;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-public class TreeUtils {}
+public class TreeUtils {
+
+  public static <T> BinaryTreeNode<T> buildTree(List<T> datas) {
+    if (datas == null || datas.size() == 0) return null;
+    BinaryTreeNode<T> root = new BinaryTreeNode<>(datas.get(0));
+    BinaryTreeNode<T> n = root;
+    Queue<BinaryTreeNode<T>> que = new LinkedList<>();
+    for (int i = 1; i < datas.size(); i++) {
+      T dt = datas.get(i);
+      if (n.left == null) {
+        n.left = new BinaryTreeNode<T>(dt);
+        que.add(n.left);
+      } else {
+        n.right = new BinaryTreeNode<T>(dt);
+        que.add(n.right);
+        n = que.remove();
+      }
+    }
+    return root;
+  }
+
+}
