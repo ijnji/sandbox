170897ec1258ce702be87016642f3fc666bf3268
ijnji <rnduan@gmail.com>
2017-05-11 05:52:09 -0400

    Update.

--- java/rlib/TreeUtils.java
+++ java/rlib/TreeUtils.java
@@ -10,60 +10,58 @@ public class TreeUtils {
   public static <T> BinaryTreeNode<T> buildTree(List<T> datas) {
     if (datas == null || datas.size() == 0) return null;
     BinaryTreeNode<T> root = new BinaryTreeNode<>(datas.get(0));
-    BinaryTreeNode<T> n = root;
     Queue<BinaryTreeNode<T>> que = new LinkedList<>();
-    for (int i = 1; i < datas.size(); i++) {
-      T dt = datas.get(i);
-      if (n.left == null) {
-        n.left = new BinaryTreeNode<T>(dt);
-        que.add(n.left);
-      } else {
-        n.right = new BinaryTreeNode<T>(dt);
-        que.add(n.right);
-        n = que.remove();
+    que.add(root);
+    int i = 1;
+    while (i < datas.size()) {
+      BinaryTreeNode<T> n = que.remove();
+      n.left = new BinaryTreeNode<T>();
+      n.left.data = (i < datas.size()) ? datas.get(i++) : null;
+      n.right = new BinaryTreeNode<T>();
+      n.right.data = (i < datas.size()) ? datas.get(i++) : null;
+      que.add(n.left);
+      que.add(n.right);
+    }
+    return trimTree(root);
+  }
+
+  private static <T> BinaryTreeNode<T> trimTree(BinaryTreeNode<T> root) {
+    if (root == null || root.data == null) return null;
+    Queue<BinaryTreeNode<T>> que = new LinkedList<>();
+    que.add(root);
+    while (que.size() > 0) {
+      BinaryTreeNode<T> n = que.remove();
+      if (n.left != null) {
+        if (n.left.data == null) n.left = null;
+        else que.add(n.left);
+      }
+      if (n.right != null) {
+        if (n.right.data == null) n.right = null;
+        else que.add(n.right);
       }
     }
-    trimTree(root);
     return root;
   }
 
   public static <T> BinaryTreeNode<T> buildTreeWithParent(List<T> datas) {
-    if (datas == null || datas.size() == 0) return null;
-    BinaryTreeNode<T> root = buildTree(datas);
-    buildParents(root);
-    return root;
+    return linkParents(buildTree(datas));
   }
 
-  private static <T> void trimTree(BinaryTreeNode<T> root) {
-    Stack<BinaryTreeNode<T>> stk = new Stack<>();
-    stk.push(root);
-    while (stk.size() > 0) {
-      BinaryTreeNode<T> n = stk.pop();
-      if (n.left != null) {
-        if (n.left.data == null) n.left = null;
-        else stk.push(n.left);
-      }
-      if (n.right != null) {
-        if (n.right.data == null) n.right = null;
-        else stk.push(n.right);
-      }
-    }
-  }
-
-  private static <T> void buildParents(BinaryTreeNode<T> root) {
-    Stack<BinaryTreeNode<T>> stk = new Stack<>();
-    stk.push(root);
-    while (stk.size() > 0) {
-      BinaryTreeNode<T> n = stk.pop();
+  private static <T> BinaryTreeNode<T> linkParents(BinaryTreeNode<T> root) {
+    Queue<BinaryTreeNode<T>> que = new LinkedList<>();
+    que.add(root);
+    while (que.size() > 0) {
+      BinaryTreeNode<T> n = que.remove();
       if (n.left != null) {
         n.left.parent = n;
-        stk.push(n.left);
+        que.add(n.left);
       }
       if (n.right != null) {
         n.right.parent = n;
-        stk.push(n.right);
+        que.add(n.right);
       }
     }
+    return root;
   }
 
 }
