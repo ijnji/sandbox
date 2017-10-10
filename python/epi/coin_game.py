1a58a9b2f76462b6526a5e59dd00a485369f53cd
Ran Duan <rnduan@gmail.com>
2017-10-09 22:48:01 -0400

    Update.

--- python/epi/coin_game.py
+++ python/epi/coin_game.py
@@ -6,19 +6,24 @@
 # choosing and keep a coin from either end of the current list. Find the maximum
 # total revenue of the first player if both players play optimally.
 
+from enum import Enum
+
+class Turn(Enum):
+    ONE = 1
+    TWO = 2
+
 def find_revenue_recursive(coins):
     memo = [[-1] * len(coins) for i in range(len(coins))]
-    def find_revenue_helper(a, b):
-        if a > b: return 0
+    def find_revenue_helper(turn, score_one, score_two, a, b):
+        if a > b: return score_one
         if memo[a][b] == -1:
-            left = coins[a] + min(
-                find_revenue_helper(a + 2, b),
-                find_revenue_helper(a + 1, b - 1)
-            )
-            right = coins[b] + min(
-                find_revenue_helper(a, b - 2),
-                find_revenue_helper(a + 1, b - 1)
-            )
+            if turn is Turn.ONE:
+                left = find_revenue_helper(Turn.TWO, score_one + coins[a], score_two, a + 1, b)
+                right = find_revenue_helper(Turn.TWO, score_one + coins[b], score_two, a, b - 1)
+            else:
+                left = find_revenue_helper(Turn.ONE, score_one, score_two + coins[a], a + 1, b)
+                right = find_revenue_helper(Turn.ONE, score_one, score_two + coins[b], a, b - 1)
             memo[a][b] = max(left, right)
         return memo[a][b]
-    return find_revenue_helper(0, len(coins) - 1)
+
+    return find_revenue_helper(Turn.ONE, 0, 0, 0, len(coins) - 1)
